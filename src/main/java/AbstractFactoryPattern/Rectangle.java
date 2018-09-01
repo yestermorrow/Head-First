dfe3fce627b6595ec7babe4c8b67296d48e839b4
yueju <yueju@2dfire.com>
2018-09-01 15:32:43 +0800

    抽象工厂模式

---
+++ src/main/java/AbstractFactoryPattern/Rectangle.java
@@ -0,0 +1,8 @@
+package AbstractFactoryPattern;
+
+public class Rectangle implements Shape{
+
+    public void draw() {
+        System.out.println("Inside Reactangle::draw()");
+    }
+}
---
+++ src/main/java/AbstractFactoryPattern/Square.java
@@ -0,0 +1,8 @@
+package AbstractFactoryPattern;
+
+public class Square implements Shape{
+
+    public void draw() {
+        System.out.println("Inside Circle::draw()");
+    }
+}
